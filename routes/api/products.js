98396884c4ba872ffea63d5095bc51ca21d4861f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-12 21:26:52 -0400

    removed login and registration routes and moved to users.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -9,12 +9,7 @@ const Grid = require("gridfs-stream");
 const crypto = require('crypto');
 
 //Load input validation
-const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
 const validateProductInput = require("../../validation/productValidation");
-
-// Load User Model
-const User = require("../../models/User");
 
 // Load Product Model
 const Product = require("../../models/Products");
@@ -54,95 +49,6 @@ router.get('/', (req, res) => {
     .catch(err => res.status(404).json({
       error: "No listed items"
     }));
-});
-
-//@route POST api/products/register
-//@desc Register new users
-//@access Public
-router.post("/register", (req, res) => {
-  //Form validation
-
-  const  { errors, isValid } = validateRegisterInput(req.body)
-
-  //Check Validation
-  if(!isValid){
-    return res.status(400).json(errors);
-  }
-  User.findOne({ email: req.body.email }).then(user =>{
-    if(user){
-      return res.status(400).json({ email: "Email already exists" })
-    }
-    const newUser = new User({
-      name: req.body.name,
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password
-    });
-    // Hash password before saving in database
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err,hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser.save()
-        .then(user => res.json(user))
-        .catch(err => console.log(err));
-      });
-    });
-  });  
-});
-
-//@route POST api/products/login
-//@desc Login user and return JWT token
-//@access Public
-
-router.post("/login", (req, res) => {
-  //Form validation
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  //Check validaiton
-  if(!isValid){
-    return res.status(400).json(errors);
-  }
-  const email = req.body.email;
-  const password = req.body.password;
-
-  //Find by user email
-  User.findOne({ email }).then(user => {
-    //Check if email exists
-    if(!user){
-      return res.status(404).json({ emailNotFound: "Email not found" });
-    }
-    //Check password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if(isMatch){
-        //User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name
-        };
-        //Sign token
-        jwt.sign(
-          payload,
-          keys.secretOrKey,
-          {
-            expiresIn: 31556926 // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: "Bearer " + token
-            });
-          }
-        );
-      } else {
-        return res
-        .status(400)
-        .json({ passwordIncorrect: "Incorrect password" })
-      }
-    });
-  });
 });
 
 // @route GET api/products/name
