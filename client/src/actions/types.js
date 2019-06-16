830cbb40ca590459bb2e62dce69f6047c096b9eb
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-16 17:38:21 -0400

    added action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,9 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const USER_LOADING = "USER_LOADING";
 export const SET_CURRENT_USER = "SET_CURRENT_USER";
+export const GET_PRODUCTS = "GET_PRODUCTS";
+export const ADD_PRODUCT = "ADD_PRODUCT";
+export const DELETE_PRODUCTS = "DELETE_ITEM";
+export const PRODUCTS_LOADING = "PRODUCTS_LOADING";
+
 
