adcd63c26461bc8a0ae3f86604ad9f3ece5f0871
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-06-16 17:42:47 -0400

    created productReducer.js

---
+++ client/src/reducers/productReducer.js
@@ -0,0 +1,39 @@
+import {
+    GET_PRODUCTS,
+    ADD_PRODUCT,
+    DELETE_PRODUCTS,
+    PRODUCTS_LOADING
+} from "../actions/types";
+
+const initialState = {
+    products: [],
+    loading: false
+};
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case GET_PRODUCTS:
+            return {
+                ...state,
+                products: action.payload,
+                loading: true
+            };
+        case DELETE_PRODUCTS:
+            return {
+                ...state,
+                products: state.products.filter(product => product._id !== action.payload)
+            };
+        case ADD_PRODUCT:
+            return {
+                ...state,
+                products: [action.payload, ...state.products]
+            };
+        case PRODUCTS_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+        default:
+            return state;
+    }
+}
