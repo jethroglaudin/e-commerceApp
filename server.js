05f620eb2ba62d47d8efe7fcaa3e45209e090ad0
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-12-03 21:30:51 -0500

    initialized chalk for console log messages

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const keys = require("./config/keys");
 const port = process.env.Port || 5000;
 const product = require("./routes/api/products");
 const users = require("./routes/api/users");
+const chalk = require("chalk");
 
 const app = express();
 
@@ -23,7 +24,7 @@ mongoose
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
-    console.log(`MongoDB connected`);
+    console.log(chalk.inverse.green(`MongoDB connected`));
   })
   .catch(err => console.log(err));
 
@@ -36,5 +37,5 @@ app.use("/api/products", product);
 
 app.listen(port, err => {
   if (err) throw err;
-  console.log(`App is live currently listening on port ${port}`);
+  console.log(chalk.inverse.blue(`App is live currently listening on port ${port}`));
 });
