a46b0f75e0653333d8fc0a61122eb0d208cc1b36
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-02 23:31:09 -0500

    map current state to auth props

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { logoutUser } from "../../actions/authActions";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
 
 class Navbar extends Component {
+    onLogoutClick(e) {
+        e.preventDefault();
+        this.props.logoutUser();
+    }
+
+
     render() {
+        const { isAuthenticated } = this.props.auth;
+
+        const authenticatedNav = (
+            <ul className="navbar-fixed">
+                <nav className="z-depth-0">
+                    <li className="nav-item">
+                        <Link className="nav-link">
+                        Buy Product
+                        </Link>
+                    </li>
+                    <div className="nav-wrapper white">
+
+                    </div>
+                </nav>
+            </ul>
+        )
         return (
             <div className="navbar-fixed">
                 <nav className="z-depth-0">
@@ -24,4 +50,15 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
+})
+
+export default connect(
+    mapStateToProps, { logoutUser }
+)(Navbar);
