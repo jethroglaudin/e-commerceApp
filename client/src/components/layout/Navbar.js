331a04568b413bb800e8ac1d6649f91233b62f50
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2020-01-03 16:40:47 -0500

    updated Navbar to have two seperate versions depending on if the user is logged in or out

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,61 +4,76 @@ import { logoutUser } from "../../actions/authActions";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-
 class Navbar extends Component {
-    onLogoutClick(e) {
-        e.preventDefault();
-        this.props.logoutUser();
-    }
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+  }
 
+  render() {
+    const { isAuthenticated, user } = this.props.auth;
 
-    render() {
-        const { isAuthenticated } = this.props.auth;
+    const authenticatedNav = (
+      <div className="navbar-fixed">
+        <nav className="z-depth-3">
+          <li className="nav-wrapper white">
+            <Link
+              to="/"
+              style={{
+                fontFamily: "monospace"
+              }}
+              className="col s5 brand-logo center black-text"
+            >
+              <i className="material-icons">shop</i>
+              Shop-It
+            </Link>
+          </li>
 
-        const authenticatedNav = (
-            <ul className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <li className="nav-item">
-                        <Link className="nav-link">
-                        Buy Product
-                        </Link>
-                    </li>
-                    <div className="nav-wrapper white">
-
-                    </div>
-                </nav>
-            </ul>
-        )
-        return (
-            <div className="navbar-fixed">
-                <nav className="z-depth-0">
-                    <div className="nav-wrapper white">
-                        <Link
-                            to="/"
-                            style={{
-                                fontFamily: "monospace"
-                            }}
-                            className="col s5 brand-logo center black-text"
-                        >
-                            <i className="material-icons">shop</i>
-                            Shop-It
-                        </Link>
-                    </div>
-                </nav>
+          <div className="row">
+            <div className="col s12">
+              <ul className="tabs">
+                <li className="tab col s3">
+                  <Link to="#">Buy Products</Link>
+                </li>
+                <li className="tab col s3">
+                  <Link to="/Post">List Product</Link>
+                </li>
+                <li className="tab col s2" style={{color: "black"}}>
+                  <b>Hello,</b> {user.name.split(" ")[0]}
+                </li>
+                <li onClick={this.onLogoutClick} class="tab col s3">
+                  <a href="#test3">Logout</a>
+                </li>
+              </ul>
             </div>
-        );
-    }
+          </div>
+        </nav>
+      </div>
+    );
+
+    const unAuthenticatedNav = (
+      <ul className="">
+        <nav>
+          <li>Time</li>
+        </nav>
+      </ul>
+    );
+
+    return (
+      <div className="navbar-fixed">
+        {isAuthenticated ? authenticatedNav : unAuthenticatedNav}
+      </div>
+    );
+  }
 }
 
 Navbar.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
-}
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
 const mapStateToProps = state => ({
-    auth: state.auth
-})
+  auth: state.auth
+});
 
-export default connect(
-    mapStateToProps, { logoutUser }
-)(Navbar);
+export default connect(mapStateToProps, { logoutUser })(Navbar);
